d08a9e0927d800431b43701f39f2dabc7ddb9251
Karthik S <73390717+karthiks2611@users.noreply.github.com>
2021-10-17 15:11:09 +0530

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
    $host        = "host = ec2-52-87-123-108.compute-1.amazonaws.com";
    $port        = "port = 5432";
    $dbname      = "dbname = d8sqb85i5r50f3";
-   $credentials = "user = postgres password=f2f23703d8d27d8ee39d40dc344705dd67358848407a7c712ea80ebcde2363ce";
+   $credentials = "user = fstnivqytebnti password=f2f23703d8d27d8ee39d40dc344705dd67358848407a7c712ea80ebcde2363ce";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
