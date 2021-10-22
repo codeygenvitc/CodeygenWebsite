e2ad2585b584dbe2d05afab4ef26157e1d6eb8f7
codeygenvitc <83817164+codeygenvitc@users.noreply.github.com>
2021-10-22 19:01:54 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,8 @@
    $port        = 5432;
    $dbname      = getenv('DB_NAME');
    $credentials = getenv('DB_USER');
+   echo "$host $port $dbname $credentials";
+   echo "\n";
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
       echo "Error : Unable to open database\n";
